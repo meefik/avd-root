41f331fd677881262ff05820a1aebd4703d5b8fd
meefik <meefik@gmail.com>
2019-11-01 19:41:57 +0300

    Update the script

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # avd-root
+
 Get root permissions in Android Virtual Devices.
 
-### Usage
+## Usage
 
-    ./avd-root.sh <arm|x86>
+Find out the name of AVD:
+
+```sh
+emulator -list-avds
+```
+
+Start the emulator with the name of AVD:
+
+```sh
+emulator -avd "AVD_NAME_HERE" -selinux permissive
+```
+
+After starting AVD run the script:
+
+```sh
+./avd-root.sh
+```
--- avd-root.sh
+++ avd-root.sh
@@ -1,18 +1,45 @@
-#!/bin/bash -e
-ARCH=$1
-if [ "$#" -ne 1 ]
-then
-    echo "Usage: $0 <arm|x86>"
-    exit 1
-fi
+#!/bin/sh
+# List of AVDs:
+# $ emulator -list-avds
+# Start the emulator without SELinux:
+# $ emulator -avd Android_API_29 -selinux permissive
+# Related links:
+# https://download.chainfire.eu/supersu/
+# http://4pda.ru/forum/index.php?showtopic=318487
+
+set -e -x
+
+BIN_DIR="/system/xbin"
+SU_ARC="SR5-SuperSU-v2.82-SR5-20171001224502.zip"
+SU_APK="SuperSU-v2.82-SR5.apk"
+
+ARCH=$(adb shell getprop ro.product.cpu.abi | tr -d '\n\r')
+case "$ARCH" in
+arm64|aarch64)
+  ARCH="arm64"
+;;
+arm*)
+  ARCH="arm"
+;;
+x86_64|amd64)
+  ARCH="x64"
+;;
+i[3-6]86|x86)
+  ARCH="x86"
+;;
+esac
+
 adb root
-adb push ./bb/$ARCH/busybox /data/local/busybox
-adb shell "chmod 755 /data/local/busybox"
-adb shell "/data/local/busybox mount -o rw,remount /system"
-adb shell "/data/local/busybox --install -s /system/xbin"
-adb shell "rm -r /system/media/audio"
-adb push ./su/$ARCH/bin/su /system/bin/su
-adb shell "chown 0:0 /system/bin/su"
-adb shell "chmod 6755 /system/bin/su"
-adb install ./su/$ARCH/app/Superuser.apk
+adb shell "grep -q $BIN_DIR /proc/mounts || mount -t tmpfs -o size=50M tmpfs $BIN_DIR"
+
+[ -e "/tmp/su" ] || mkdir -p /tmp/su
+unzip -o "$SU_ARC" -d /tmp/su >/dev/null
+if [ -e "/tmp/su/$ARCH/su.pie" ]
+then
+  mv /tmp/su/$ARCH/su.pie /tmp/su/$ARCH/su
+fi
+adb push /tmp/su/$ARCH/* $BIN_DIR
+adb shell "$BIN_DIR/su --daemon&"
+adb install "$SU_APK"
+
 exit 0
